c7819e9d94886ec084524579968aeb3ad0de01e1
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-01-02 11:21:12 +0100

    Ajout d'un PHPDoc dans les classes attributs des Relations

--- src/Relation/ARelation.php
+++ src/Relation/ARelation.php
@@ -7,13 +7,25 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class ARelation implements IARelation
 {
+    /** @var ERelation Relation type associated with the property.*/
     private ERelation $type;
 
+
+    /**
+     * Creates a relation attribute with a specific relation type.
+     *
+     * @param ERelation $type The relation type of the property
+     */
     public function __construct(Erelation $type) {
         $this->type = $type;
     }
+
+    /**
+     * Returns the relation type of the property.
+     *
+     * @return ERelation
+     */
     public function getType():ERelation {
         return $this->type;
     }
-
 }
--- src/Relation/ARelationField.php
+++ src/Relation/ARelationField.php
@@ -4,11 +4,15 @@ namespace OrmLibrary\Relation;
 
 use Attribute;
 use OrmLibrary\Field\AField;
-use OrmLibrary\Field\IField;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class ARelationField extends AField implements IARelation
 {
+    /**
+     * Returns the type of relationship represented by this field.
+     *
+     * @return ERelation The relation type (Many-To-One)
+     */
     public function getType():ERelation {
         return ERelation::MTO;
     }
