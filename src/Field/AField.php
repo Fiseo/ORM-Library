b92c5b3bc543aadeb69535a8c872f37dd3bd22f6
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-01-02 10:28:38 +0100

    Ajout d'un PHPDoc dans la classe AField

--- src/Field/AField.php
+++ src/Field/AField.php
@@ -7,18 +7,37 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class AField
 {
+    /** @var string Database field name */
     private string $name;
+
+    /** @var bool Indicates whether the field allows NULL values */
     private bool $nullable;
 
+    /**
+     * Creates a new field mapping attribute.
+     *
+     * @param string $name     The database column name.
+     * @param bool   $nullable Whether the field allows NULL values.
+     */
     public function __construct(string $name, bool $nullable) {
         $this->name = $name;
         $this->nullable = $nullable;
     }
 
+    /**
+     * Returns the database column name.
+     *
+     * @return string The field name.
+     */
     public function getName():string {
         return $this->name;
     }
 
+    /**
+     * Indicates whether the field allows NULL values.
+     *
+     * @return bool True if the field is nullable, false otherwise.
+     */
     public function isNullable():bool {
         return $this->nullable;
     }
