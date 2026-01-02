15e2c90fcd49c7b97335a71a5c5ef225c0ae3f9d
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-01-02 11:46:29 +0100

    Ajout d'un PHPDoc dans la classe RelationOTM

--- src/Relation/RelationOTM.php
+++ src/Relation/RelationOTM.php
@@ -10,10 +10,23 @@ use OrmLibrary\Query\Where;
 
 class RelationOTM implements IRelation
 {
+    /** @var AbstractEntity The owner entity of this relation. */
     private AbstractEntity $owner;
+
+    /** @var string The fully qualified class name of the related entity. */
     private string $relation;
+
+    /** @var AbstractEntity[] The cached list of related entities. */
     private array $list;
 
+    /**
+     * Initializes the OTM relation.
+     *
+     * @param AbstractEntity $owner The owner entity.
+     * @param string $fqcn Fully qualified class name of the related entity.
+     *
+     * @throws Exception If $fqcn is not a child of AbstractEntity.
+     */
     public function __construct(AbstractEntity $owner, string $fqcn) {
         if (!Helpers::isEntity($fqcn))
             throw new Exception("A relation must be with a child entity of AbstractEntity class");
@@ -21,6 +34,17 @@ class RelationOTM implements IRelation
         $this->relation = $fqcn;
     }
 
+    /**
+     * Retrieves the related entities.
+     *
+     * Lazy-loads the entities from the database if needed.
+     *
+     * @param bool $reload If true, forces a reload from the database even if cached.
+     *
+     * @return AbstractEntity[] The list of related entities.
+     *
+     * @throws Exception If the relation cannot be resolved via the repository.
+     */
     public function get(bool $reload = false):array {
         if (!$reload && !empty($this->list))
             return $this->list;
@@ -38,6 +62,15 @@ class RelationOTM implements IRelation
         return $this->list;
     }
 
+    /**
+     * Retrieves the related entities and ensures they are fully loaded.
+     *
+     * @param bool $reload If true, forces a reload from the database even if cached.
+     *
+     * @return AbstractEntity[] The list of fully loaded related entities.
+     *
+     * @throws Exception If the relation cannot be resolved via the repository
+     */
     public function getLoaded(bool $reload):array {
         $this->get($reload);
         foreach ($this->list as $item) {
